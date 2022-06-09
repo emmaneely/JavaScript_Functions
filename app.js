66bd2bba5aa449bc037946adfc83005d748f9fb8
emmaneely <emmaneely3@gmail.com>
2022-06-09 15:12:18 -0500

    Completed exercise 1

--- app.js
+++ app.js
@@ -3,5 +3,16 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) {
+    for (let i = 1; i <= count; i++) {
+        if (i % 2 !== 0) {
+            console.log(i);
+        } else {
+            continue;
+        }
+    }
+}
+// printOdds(10);
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
