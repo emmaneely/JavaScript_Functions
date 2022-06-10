e047cb91a8fed68f7d5495a2813f6c914d934cc9
emmaneely <emmaneely3@gmail.com>
2022-06-09 22:54:22 -0500

    Completed exercise 3

--- app.js
+++ app.js
@@ -31,4 +31,28 @@ function checkAge(age) {
         console.log("That's not a valid age!");
     }
 }
-checkAge(16);
+// checkAge(16);
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
+function quadFinder(x, y) {
+    if (x > 0 && y > 0) {
+        console.log("Quadrant 1");
+    } else if (x < 0 && y > 0) {
+        console.log("Quadrant 2");
+    } else if (x < 0 && y < 0) {
+        console.log("Quadrant 3");
+    } else if (x > 0 && y < 0) {
+        console.log("Quadrant 4");
+    } else if (x == 0 && y == 0) {
+        console.log("You found the origin!");
+    } else {
+        console.log("Not valid coordinates!");
+    }
+}
+quadFinder(1, 2);
+// x +, y + = 1
+// x -, y + = 2
+// x -, y - = 3
+// x +, y - = 4
