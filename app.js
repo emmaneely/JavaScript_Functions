de9b6ce635b33587e200b2adab08c38fb210be8d
emmaneely <emmaneely3@gmail.com>
2022-06-09 22:52:52 -0500

    Completed exercise 2 and added exercise 1 bonus

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ console.log("Hello World!\n==========\n");
 console.log("EXERCISE 1:\n==========\n");
 
 function printOdds(count) {
+    if (count < 0) {
+        count = count * -1;
+    }
     for (let i = 1; i <= count; i++) {
         if (i % 2 !== 0) {
             console.log(i);
@@ -16,3 +19,16 @@ function printOdds(count) {
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(age) {
+    let aboveSixteen = "You can drive!";
+    let belowSixteen = "Sorry, but you need to wait until you're 16.";
+    if (age < 16) {
+        console.log(belowSixteen);
+    } else if (age >= 16) {
+        console.log(aboveSixteen);
+    } else {
+        console.log("That's not a valid age!");
+    }
+}
+checkAge(16);
